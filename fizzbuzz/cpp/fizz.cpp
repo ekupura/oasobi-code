663d8686cbba68fee3847164b9c2b3b2d38f120d
in9b <asayuuya.m9@gmail.com>
2016-06-18 04:35:11 +0900

    cpp

---
+++ fizzbuzz/cpp/fizz.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    for(int i=1;i<101;i++){
+        if(!(i % 15)) std::cout << "fizzbuzz" << std::endl;
+        else if(!(i % 3)) std::cout << "fizz" << std::endl;
+        else if(!(i % 5)) std::cout << "buzz" << std::endl;
+        else std::cout << i << std::endl;
+    }
+}
